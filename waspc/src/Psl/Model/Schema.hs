5d44a2ed12da474a6eb2ed4fb12d366ae3b4d9b7
Martin Sosic <sosic.martin@gmail.com>
2020-12-18 11:23:04 +0100

    wip

--- waspc/src/Psl/Model/Schema.hs
+++ waspc/src/Psl/Model/Schema.hs
@@ -8,7 +8,11 @@ import PslModelAst
 astToSchema :: Model -> String
 astToSchema = error "TODO"
 
+
+
 -- TODO: I should make sure to skip attributes that are not known in prisma.
+--   Or maybe it would be better if that was done in previous step, where
+--   we basically edit the AST by kicking out those attributes.
 
 
 -- TODO: Rename PslModelAst module to Psl.Model.Ast .
